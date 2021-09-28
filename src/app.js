d7f0a05ace6758364aa6e1080fb6985232d67c6a
ricardodsb <ricardojdasilva15@gmail.com>
2021-09-28 19:41:28 +0000

    final changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#domain").innerHTML = generateDomain();
+};
+const generateDomain = () => {
+  let pronoun = ["the", "our", "my"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".es", ".net"];
+  let result = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let x = 0; x < dom.length; x++) {
+          result.push(pronoun[i] + adj[j] + noun[y] + dom[x]);
+        }
+      }
+    }
+  }
+  let index = Math.floor(Math.random() * result.length);
+  return result[index];
 };
